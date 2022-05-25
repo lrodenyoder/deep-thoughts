d0d487f005f161cee4109a287d92f7222f0b2102
lrodenyoder <rodenlau000@gmail.com>
2022-05-25 17:57:18 -0500

    21.3.6

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 //create new Apollo server and pass in schema data
@@ -23,6 +23,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   //integrate apollo server with the Express application as middleware
   server.applyMiddleware({ app });
+
+  //serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   db.once("open", () => {
     app.listen(PORT, () => {
