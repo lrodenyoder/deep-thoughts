02fd27c92b45ac7885d27488f1998c14e283d91a
lrodenyoder <rodenlau000@gmail.com>
2022-05-30 16:30:06 -0500

    21.5

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 
@@ -31,6 +32,13 @@ const Profile = (props) => {
           <ThoughtList
             thoughts={user.thoughts}
             title={`${user.username}'s thoughts...`}
+          />
+        </div>
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
           />
         </div>
       </div>
